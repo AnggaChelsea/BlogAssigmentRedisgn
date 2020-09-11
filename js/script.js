47f2c66b001f2c7ae98c629840f42a45b834a36c
Angga Chelsea <muhidabdul168@gmail.com>
2020-09-11 10:34:35 +0700

    aboutMe.html

--- js/script.js
+++ js/script.js
@@ -73,13 +73,22 @@ $("#form-comment").on("submit", function (event) {
 });
 
 //membuat built in functions
-const inputValue = document.getElementById("need");
-const lenghtValue = inputValue.length;
 
-if (lenghtValue === 12) {
-  document.write(`<i class="gg-arrow-align-h"></i>`)
-} else {
-  document.write("Too many words")
+//ReadMore
+function myReadMore() {
+  let dots = document.getElementById("dots");
+  let moreText = document.getElementById("more");
+  let btnText = document.getElementById("myBtn");
+
+  if (dots.style.display === "none") {
+    dots.style.display = "inline";
+    btnText.innerHTML = "Read more";
+    moreText.style.display = "none";
+  } else {
+    dots.style.display = "none";
+    btnText.innerHTML = "Read less";
+    moreText.style.display = "inline";
+  }
 }
 
 //notifications
